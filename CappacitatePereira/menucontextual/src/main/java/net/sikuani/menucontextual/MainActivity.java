9e05435326731225555b6ba1792e0608b1bea3e2
Julian R Figueroa <julian@Julians-MacBook-Pro.local>
2015-04-10 15:38:29 -0500

    segunda sesion

--- CappacitatePereira/menucontextual/src/main/java/net/sikuani/menucontextual/MainActivity.java
+++ CappacitatePereira/menucontextual/src/main/java/net/sikuani/menucontextual/MainActivity.java
@@ -2,10 +2,13 @@ package net.sikuani.menucontextual;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,8 +34,36 @@ public class MainActivity extends ActionBarActivity {
         ListView list = (ListView)findViewById(R.id.listView);
 
         list.setAdapter(myAdapter);
+
+        registerForContextMenu(list);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.list_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()){
+            case R.id.item1:
+                msg = "item1";
+                break;
+            case R.id.item2:
+                msg = "item2";
+                break;
+            case R.id.item3:
+                msg = "item3";
+                break;
+        }
+
+        Toast.makeText(this, "Hiciste click en "+msg, Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
